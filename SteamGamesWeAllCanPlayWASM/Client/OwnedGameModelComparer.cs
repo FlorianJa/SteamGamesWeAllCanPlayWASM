e88e2dfd7e9d6e7835478335be03ddbcf0bf44bd
Florian Jasche <florian.jasche@gmail.com>
2022-01-10 15:46:15 +0100

    added library comparision

---
+++ SteamGamesWeAllCanPlayWASM/Client/OwnedGameModelComparer.cs
@@ -0,0 +1,16 @@
+﻿using Steam.Models.SteamCommunity;
+
+namespace SteamGamesWeAllCanPlayWASM.Client
+{
+    class OwnedGameModelComparer : IEqualityComparer<OwnedGameModel>
+    {
+        public bool Equals(OwnedGameModel x, OwnedGameModel y)
+        {
+            return x.AppId == y.AppId;
+        }
+        public int GetHashCode(OwnedGameModel other)
+        {
+            return other.AppId.GetHashCode();
+        }
+}
+}
--- SteamGamesWeAllCanPlayWASM/Client/Services/SteamService.cs
+++ SteamGamesWeAllCanPlayWASM/Client/Services/SteamService.cs
@@ -42,12 +42,40 @@ namespace SteamGamesWeAllCanPlayWASM.Client.Services
             var response = await _httpClient.GetAsync($"api/user/{steamId}/ownedgames");
             if (response.StatusCode == HttpStatusCode.OK)
             {
-                var text = await response.Content.ReadAsStringAsync();
                 var ownedGames = await response.Content.ReadFromJsonAsync<OwnedGamesResultModel>();
 
                 return ownedGames.OwnedGames;
             }
             return null;
+        }
+
+        public async Task<IEnumerable<OwnedGameModel>> GetComparedLibrariesAsync(IEnumerable<string> steamIds)
+        {
+            var mergedLibraries = new List<OwnedGameModel>();
+            var allGameLibraries = new List<IEnumerable<OwnedGameModel>>();
+
+            foreach (var steamId in steamIds)
+            {
+                allGameLibraries.Add(await GetOwnedGamesAsync(steamId));
+
+            }
+
+            allGameLibraries = allGameLibraries.OrderByDescending(l => l.Count()).ToList();
+
+
+            foreach (var library in allGameLibraries)
+            {
+                if (mergedLibraries.Count == 0)
+                {
+                    mergedLibraries.AddRange(library);
+                }
+                else
+                {
+                    mergedLibraries = mergedLibraries.Intersect(library, new OwnedGameModelComparer() ).ToList();
+                }
+            }
+
+            return mergedLibraries;
         }
     }
 }
